3ea1b87e8dc556160846dfdcf018636fee2125cb
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-22 00:04:28 +0100

    finished displaying genres

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -9,11 +9,11 @@ import { PlusIcon, XIcon } from "@heroicons/react/solid";
 import ReactPlayer from "react-player/lazy";
 
 function Movie({ result }) {
+    console.log(result);
     const [session] = useSession();
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
     const router = useRouter();
     const [showPlayer, setShowPlayer] = useState(false);
-
     useEffect(() => {
         if (!session) {
             router.push("/");
@@ -81,7 +81,7 @@ function Movie({ result }) {
                         <p className="text-xs md:text-sm">
                             {result.release_date || result.first_air_date} •{" "}
                             {Math.floor(result.runtime / 60)}h {result.runtime % 60}m •{" "}
-                            {/* {result.genres.map((genre) => genre.name + ", ")} */}
+                            {result.genres.map((genre) => genre.name + " ")}{" "}
                         </p>
                         <h4 className="text-sm md:text-lg max-w-4xl">{result.overview}</h4>
                     </div>
@@ -89,33 +89,34 @@ function Movie({ result }) {
                     {showPlayer && (
                         <div className="absolute inset-0 bg-black opacity-50 h-full w-full z-50"></div>
                     )}
-
-                    <div>
-            <div className="flex items-center justify-between bg-black text-[#f9f9f9] p-3.5">
-              <span className="font-semibold">Play Trailer</span>
-              <div
-                className="cursor-pointer w-8 h-8 flex justify-center items-center rounded-lg opacity-50 hover:opacity-75 hover:bg-[#0F0F0F]"
-                onClick={() => setShowPlayer(false)}
-              >
-                <XIcon className="h-5" />
-              </div>
-            </div>
-                  <div className="relative pt-[56.25%]">
-                        <ReactPlayer
-                            url={`https://www.youtube.com/watch?v=${result.videos.results[index].key}`}
-                            width="100%"
-                            height="100%"
-                            style={{ position: "absolute", top: "0", left: "0" }}
-                            controls={true}
-                            playing={showPlayer}
-                        />
+                    <div
+                        className={`absolute top-3 inset-x-[7%] md:inset-x-[13%] rounded overflow-hidden transition duration-1000 ${showPlayer ? "opacity-100 z-50" : "opacity-0"
+                            }`}
+                    >
+                        <div className="flex items-center justify-between bg-black text-[#f9f9f9] p-3.5">
+                            <span className="font-semibold">Play Trailer</span>
+                            <div
+                                className="cursor-pointer w-8 h-8 flex justify-center items-center rounded-lg opacity-50 hover:opacity-75 hover:bg-[#0F0F0F]"
+                                onClick={() => setShowPlayer(false)}
+                            >
+                                <XIcon className="h-5" />
+                            </div>
+                        </div>
+                        <div className="relative pt-[56.25%]">
+                            <ReactPlayer
+                                url={`https://www.youtube.com/watch?v=${result.videos?.results[index]?.key}`}
+                                width="100%"
+                                height="100%"
+                                style={{ position: "absolute", top: "0", left: "0" }}
+                                controls={true}
+                                playing={showPlayer}
+                            />
+                        </div>
                     </div>
-          </div>
-        </section>
-    )
-}
-    </div >
-  );
+                </section>
+            )}
+        </div>
+    );
 }
 export default Movie;
 export async function getServerSideProps(context) {
