e266e4227d9cd9ca4cd6ff261b5ba82212550987
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-21 15:48:34 +0100

    working on layout

--- components/MovieThumbnail.js
+++ components/MovieThumbnail.js
@@ -1,21 +1,21 @@
-import Image from "next/image";
 
+import Image from "next/image";
 function MovieThumbnail({ result }) {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
     return (
-        <div className="min-w-[300px] min-h-[150px] rounded-3xl overflow-hidden">
-            <Image
-                src={
-                    `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
-                    `${BASE_URL}${result.poster_path}`
-                }
-                width={300}
-                height={150}
-                objectFit="cover"
-            />
-        </div>
-    );
+            <div className="min-w-[300px] min-h-[150px] rounded-md overflow-hidden cursor-pointer">
+                <Image
+                    src={
+                        `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+                        `${BASE_URL}${result.poster_path}`
+                    }
+                    width={300}
+                    height={150}
+                    objectFit="cover"
+                    className="rounded-md"
+                />
+            </div>
+            );
 }
-
-export default MovieThumbnail;
+            export default MovieThumbnail;
--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ import MovieCollection from "../components/MovieCollection";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Slider from "../components/Slider";
+import ComponentDivider from "../components/ComponentDivider";
 
 export default function Home({ nowPlaying }) {
     const [session] = useSession();
-
     return (
         <div>
             <Head>
@@ -16,36 +16,31 @@ export default function Home({ nowPlaying }) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header />
-
-              {!session ? (
+            {!session ? (
                 <Hero />
             ) : (
-                    <main className="relative min-h-screen">
-                        <div className="bg-home bg-center bg-cover bg-no-repeat bg-fixed absolute inset-0 ">
-                            <Slider />
-                            <Brands />
-                            {/* <section className=" max-w-[1370px] mx-auto flex flex-col justify-center items-center mt-14"> */}
-                            <MovieCollection results={nowPlaying} />
-                            <MovieCollection results={nowPlaying} />
-                            {/* </section> */}
-                        </div>
+                    <main className="relative min-h-screen after:bg-home after:bg-center after:bg-cover after:bg-no-repeat after:bg-fixed after:absolute after:inset-0 after:z-[-1]">
+                        <Slider />
+                        <Brands />
+                        {/* <section className=" max-w-[1370px] mx-auto flex flex-col justify-center items-center mt-14"> */}
+                        <MovieCollection results={nowPlaying} />
+                        <ComponentDivider />
+                        <MovieCollection results={nowPlaying} />
+                        {/* </section> */}
                     </main>
-            )}
-        </div>
-    );
+      )}
+    </div>
+            );
 }
-
-
-export async function getServerSideProps(context) {
-    const session = await getSession(context);
-    const request = await fetch(
-        `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en-US&page=1`
-    ).then((response) => response.json());
-
-    return {
-        props: {
-            session,
-            nowPlaying: request.results,
-        },
-    };
+            export async function getServerSideProps(context) {
+  const session = await getSession(context);
+            const request = await fetch(
+            `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en-US&page=1`
+  ).then((response) => response.json());
+            return {
+                props: {
+                session,
+                nowPlaying: request.results,
+    },
+  };
 }
