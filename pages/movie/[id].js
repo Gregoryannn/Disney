f9c12637ed40c79e2bc119e442f0f98e25092ad5
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-21 23:16:49 +0100

    working on trailer functionality

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -6,10 +6,9 @@ import { useEffect } from "react";
 import Header from "../../components/Header";
 import Hero from "../../components/Hero";
 import { PlusIcon } from "@heroicons/react/solid";
-import humanizeDuration from "humanize-duration";
+import ReactPlayer from "react-player/lazy";
 
 function Movie({ result }) {
-    console.log(result);
     const [session] = useSession();
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
     const router = useRouter();
@@ -18,6 +17,10 @@ function Movie({ result }) {
             router.push("/");
         }
     }, []);
+
+    const index = result.videos.results.findIndex(
+        (element) => element.type === "Trailer"
+    );
 
     return (
         <div>
@@ -30,73 +33,78 @@ function Movie({ result }) {
                 <Hero />
             ) : (
                 <section className="relative">
-                        <div className="relative min-h-[calc(100vh-72px)]">
-                            <Image
-                                src={
-                                    `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
-                                    `${BASE_URL}${result.poster_path}`
-                                }
-                                layout="fill"
-                                objectFit="cover"
-                            />
+                    <div className="relative min-h-[calc(100vh-72px)]">
+                        <Image
+                            src={
+                                `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+                                `${BASE_URL}${result.poster_path}`
+                            }
+                            layout="fill"
+                            objectFit="cover"
+                        />
+                    </div>
+                    <div className="absolute inset-y-28 md:inset-y-auto md:bottom-10 inset-x-4 md:inset-x-12 space-y-6">
+                        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
+                            {result.title || result.original_name}
+                        </h1>
+                        <div className="flex items-center space-x-3 md:space-x-5">
+                            <button className="text-xs md:text-base bg-[#f9f9f9] text-black flex items-center justify-center py-2.5 px-6 rounded hover:bg-[#c6c6c6]">
+                                <img
+                                    src="/images/play-icon-black.svg"
+                                    alt=""
+                                    className="h-6 md:h-8"
+                                />
+                                <span className="uppercase font-medium tracking-wide">
+                                    Play
+                                </span>
+                            </button>
+                            <button className="text-xs md:text-base bg-black/30 text-[#f9f9f9] border border-[#f9f9f9] flex items-center justify-center py-2.5 px-6 rounded hover:bg-[#c6c6c6]">
+                                <img
+                                    src="/images/play-icon-white.svg"
+                                    alt=""
+                                    className="h-6 md:h-8"
+                                />
+                                <span className="uppercase font-medium tracking-wide">
+                                    Trailer
+                                </span>
+                            </button>
+                            <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
+                                <PlusIcon className="h-6" />
+                            </div>
+                            <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
+                                <img src="/images/group-icon.svg" alt="" />
+                            </div>
                         </div>
-                              <div className="absolute inset-y-28 md:inset-y-auto md:bottom-10 inset-x-4 md:inset-x-12 space-y-6">
-                                    <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
-                                        {result.title || result.original_name}
-                                    </h1>
-                                        <div className="flex items-center space-x-3 md:space-x-5">
-                                                <button className="text-xs md:text-base bg-[#f9f9f9] text-black flex items-center justify-center py-2.5 px-6 rounded hover:bg-[#c6c6c6]">
-                                                    <img
-                                                        src="/images/play-icon-black.svg"
-                                                        alt=""
-                                                        className="h-6 md:h-8"
-                                                    />
-                                                    <span className="uppercase font-medium tracking-wide">
-                                                        Play
-                                                    </span>
-                                                </button>
 
-                                              <button className="text-xs md:text-base bg-black/30 text-[#f9f9f9] border border-[#f9f9f9] flex items-center justify-center py-2.5 px-6 rounded hover:bg-[#c6c6c6]">
-                                                        <img
-                                                            src="/images/play-icon-white.svg"
-                                                            alt=""
-                                                            className="h-6 md:h-8"
-                                                        />
-                                                        <span className="uppercase font-medium tracking-wide">
-                                                            Trailer
-                                                        </span>
-                                                    </button>
-                                                    <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
-                                                        <PlusIcon className="h-6" />
-                                                    </div>
-                                                    <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
-                                                        <img src="/images/group-icon.svg" alt="" />
-                                                    </div>
-            </div>
+                        <p className="text-xs md:text-sm">
+                            {result.release_date || result.first_air_date} •{" "}
+                            {Math.floor(result.runtime / 60)}h {result.runtime % 60}m •{" "}
+                            {/* {result.genres.map((genre) => genre.name + ", ")} */}
+                        </p>
+                        <h4 className="text-sm md:text-lg max-w-4xl">{result.overview}</h4>
+                    </div>
+                </section>
+            )}
 
-                                                <p className="text-xs md:text-sm">
-                                                    {result.release_date || result.first_air_date} •{" "}
-                                                    {humanizeDuration(result.runtime * 60000)} •{" "}
-                                                    {/* {result.genres.map((genre) => genre.name + ", ")} */}
-                                                </p>
-                                                <h4 className="text-sm md:text-lg max-w-4xl">{result.overview}</h4>
-                                            </div>
-        </section>
-      )}
-                                    </div>
-                                    );
+            <ReactPlayer
+                url={`https://www.youtube.com/watch?v=${result.videos.results[index].key}`}
+            />
+        </div>
+    );
 }
-                                    export default Movie;
-                                    export async function getServerSideProps(context) {
-  const session = await getSession(context);
-                                    const {id} = context.query;
-                                    const request = await fetch(
-                                    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=en-US`
-  ).then((response) => response.json());
-                                    return {
-                                        props: {
-                                        session,
-                                        result: request,
-    },
-  };
+export default Movie;
+export async function getServerSideProps(context) {
+    const session = await getSession(context);
+    const { id } = context.query;
+
+    const request = await fetch(
+            `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=en-US&append_to_response=videos`
+    ).then((response) => response.json());
+
+    return {
+        props: {
+            session,
+            result: request,
+        },
+    };
 }
