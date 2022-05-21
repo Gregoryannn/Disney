47aa6a149b11aed092e08a9eea8de15a4692dbb9
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-22 00:00:29 +0100

    working on finishing touches ✨

--- components/Header.js
+++ components/Header.js
@@ -7,64 +7,63 @@ import {
 } from "@heroicons/react/solid";
 import { signIn, signOut, useSession } from "next-auth/client";
 import { useRouter } from "next/router";
-
 function Header() {
     const [session] = useSession();
     const router = useRouter();
 
     return (
-        <header className="sticky bg-[#040714] top-0 z-50 flex items-center px-10 md:px-12 h-[72px]">
-            <Image
-                src="/images/logo.svg"
-                alt=""
-                width={80}
-                height={80}
-                className="cursor-pointer"
-                onClick={() => router.push("/")}
-            />
-            {session && (
-                <div className="hidden ml-10 md:flex items-center space-x-6">
-                    <a className="header-link group">
-                        <HomeIcon className="h-4" />
-                        <span className="uppercase text-sm font-medium">Home</span>
-                    </a>
-                    <a className="header-link">
-                        <SearchIcon className="h-4" />
-                        <span className="uppercase text-sm font-medium">Search</span>
-                    </a>
-                    <a className="header-link">
-                        <PlusIcon className="h-4" />
-                        <span className="uppercase text-sm font-medium">Watchlist</span>
-                    </a>
-                    <a className="header-link">
-                        <StarIcon className="h-4" />
-                        <span className="uppercase text-sm font-medium">Originals</span>
-                    </a>
-                    <a className="header-link">
-                        <img src="/images/movie-icon.svg" alt="" className="h-5" />
-                        <span className="uppercase text-sm font-medium">Movies</span>
-                    </a>
-                    <a className="header-link">
-                        <img src="/images/series-icon.svg" alt="" className="h-5" />
-                        <span className="uppercase text-sm font-medium">Series</span>
-                    </a>
-                </div>
-            )}
-            {!session ? (
-                <button
-                    className="ml-auto uppercase border px-4 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-black transition duration-200"
-                    onClick={signIn}
-                >
-                    Login
-                </button>
-            ) : (
-                <img
-                    src={session.user.image}
-                    className="ml-auto h-12 w-12 rounded-full object-cover cursor-pointer"
-                    onClick={signOut}
+            <header className="sticky bg-[#040714] top-0 z-[1000] flex items-center px-10 md:px-12 h-[72px]">
+                <Image
+                    src="/images/logo.svg"
+                    alt=""
+                    width={80}
+                    height={80}
+                    className="cursor-pointer"
+                    onClick={() => router.push("/")}
                 />
-            )}
-        </header>
-    );
+                {session && (
+                    <div className="hidden ml-10 md:flex items-center space-x-6">
+                        <a className="header-link group">
+                            <HomeIcon className="h-4" />
+                            <span className="uppercase text-sm font-medium">Home</span>
+                        </a>
+                        <a className="header-link">
+                            <SearchIcon className="h-4" />
+                            <span className="uppercase text-sm font-medium">Search</span>
+                        </a>
+                        <a className="header-link">
+                            <PlusIcon className="h-4" />
+                            <span className="uppercase text-sm font-medium">Watchlist</span>
+                        </a>
+                        <a className="header-link">
+                            <StarIcon className="h-4" />
+                            <span className="uppercase text-sm font-medium">Originals</span>
+                        </a>
+                        <a className="header-link">
+                            <img src="/images/movie-icon.svg" alt="" className="h-5" />
+                            <span className="uppercase text-sm font-medium">Movies</span>
+                        </a>
+                        <a className="header-link">
+                            <img src="/images/series-icon.svg" alt="" className="h-5" />
+                            <span className="uppercase text-sm font-medium">Series</span>
+                        </a>
+                    </div>
+                )}
+                {!session ? (
+                    <button
+                        className="ml-auto uppercase border px-4 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-black transition duration-200"
+                        onClick={signIn}
+                    >
+                        Login
+                    </button>
+                ) : (
+                    <img
+                        src={session.user.image}
+                        className="ml-auto h-12 w-12 rounded-full object-cover cursor-pointer"
+                        onClick={signOut}
+                    />
+                )}
+            </header>
+            );
 }
-export default Header;
+            export default Header;
--- components/ShowsCollection.js
+++ components/ShowsCollection.js
@@ -1,15 +1,15 @@
 import ShowThumbnail from "./ShowThumbnail";
 
-function ShowsCollection({ results }) {
-    return (
+    function ShowsCollection({ results, title }) {
+        return (
             <div className="flex flex-col space-y-2 my-8 px-8 max-w-[1400px] mx-auto">
-                <h2 className="font-semibold">Popular Shows</h2>
-                    <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 -m-2">
-                        {results.map((result) => (
-                            <ShowThumbnail key={result.id} result={result} />
-                        ))}
-                    </div>
+                <h2 className="font-semibold">{title}</h2>
+                <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 -m-2">
+                    {results.map((result) => (
+                        <ShowThumbnail key={result.id} result={result} />
+                    ))}
                 </div>
-                );
-}
-                export default ShowsCollection;
+            </div>
+        );
+    }
+    export default ShowsCollection;
