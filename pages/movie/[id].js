3142636af81f4a4426934d22e07e1c6d0d49f80e
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-21 23:22:10 +0100

    added close button for trailer

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -2,26 +2,26 @@ import { getSession, useSession } from "next-auth/client";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Hero from "../../components/Hero";
-import { PlusIcon } from "@heroicons/react/solid";
+import { PlusIcon, XIcon } from "@heroicons/react/solid";
 import ReactPlayer from "react-player/lazy";
 
 function Movie({ result }) {
     const [session] = useSession();
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
     const router = useRouter();
+    const [showPlayer, setShowPlayer] = useState();
+
     useEffect(() => {
         if (!session) {
             router.push("/");
         }
     }, []);
-
     const index = result.videos.results.findIndex(
         (element) => element.type === "Trailer"
     );
-
     return (
         <div>
             <Head>
@@ -58,53 +58,67 @@ function Movie({ result }) {
                                     Play
                                 </span>
                             </button>
-                            <button className="text-xs md:text-base bg-black/30 text-[#f9f9f9] border border-[#f9f9f9] flex items-center justify-center py-2.5 px-6 rounded hover:bg-[#c6c6c6]">
-                                <img
-                                    src="/images/play-icon-white.svg"
-                                    alt=""
-                                    className="h-6 md:h-8"
-                                />
-                                <span className="uppercase font-medium tracking-wide">
-                                    Trailer
-                                </span>
-                            </button>
-                            <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
-                                <PlusIcon className="h-6" />
-                            </div>
-                            <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
-                                <img src="/images/group-icon.svg" alt="" />
-                            </div>
+
+                                <button
+                                    className="text-xs md:text-base bg-black/30 text-[#f9f9f9] border border-[#f9f9f9] flex items-center justify-center py-2.5 px-6 rounded hover:bg-[#c6c6c6]"
+                                    onClick={() => setShowPlayer(true)}
+                                >
+                                    <img
+                                        src="/images/play-icon-white.svg"
+                                        alt=""
+                                        className="h-6 md:h-8"
+                                    />
+                                    <span className="uppercase font-medium tracking-wide">
+                                        Trailer
+                                    </span>
+                                </button>
+                                <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
+                                    <PlusIcon className="h-6" />
+                                </div>
+                                <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
+                                    <img src="/images/group-icon.svg" alt="" />
+                                </div>
+            </div>
+                            <p className="text-xs md:text-sm">
+                                {result.release_date || result.first_air_date} •{" "}
+                                {Math.floor(result.runtime / 60)}h {result.runtime % 60}m •{" "}
+                                {/* {result.genres.map((genre) => genre.name + ", ")} */}
+                            </p>
+                            <h4 className="text-sm md:text-lg max-w-4xl">{result.overview}</h4>
                         </div>
 
-                        <p className="text-xs md:text-sm">
-                            {result.release_date || result.first_air_date} •{" "}
-                            {Math.floor(result.runtime / 60)}h {result.runtime % 60}m •{" "}
-                            {/* {result.genres.map((genre) => genre.name + ", ")} */}
-                        </p>
-                        <h4 className="text-sm md:text-lg max-w-4xl">{result.overview}</h4>
-                    </div>
-                </section>
-            )}
+                        <div className="fixed top-20 left-44 z-50 rounded overflow-hidden">
+                            <div className="flex  items-center justify-between bg-black text-[#f9f9f9] p-3.5">
+                                <span>Trailer</span>
+                                <div className="cursor-pointer w-8 h-8 flex justify-center items-center rounded-lg opacity-50 hover:opacity-75 hover:bg-[#0F0F0F]" onClick={() => setShowPlayer(false)}>
+                                    <XIcon className="h-5" />
+                                </div>
+                            </div>
+                            <ReactPlayer
+                                url={`https://www.youtube.com/watch?v=${result.videos.results[index].key}`}
+                                width={1065}
+                                height={600}
+                                controls={true}
+                            />
+                        </div>
+        </section>
+      )}
 
-            <ReactPlayer
-                url={`https://www.youtube.com/watch?v=${result.videos.results[index].key}`}
-            />
-        </div>
-    );
+                   
+    </div>
+            );
 }
-export default Movie;
-export async function getServerSideProps(context) {
-    const session = await getSession(context);
-    const { id } = context.query;
-
-    const request = await fetch(
+            export default Movie;
+            export async function getServerSideProps(context) {
+  const session = await getSession(context);
+            const {id} = context.query;
+            const request = await fetch(
             `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=en-US&append_to_response=videos`
-    ).then((response) => response.json());
-
-    return {
-        props: {
-            session,
-            result: request,
-        },
-    };
+  ).then((response) => response.json());
+            return {
+                props: {
+                session,
+                result: request,
+    },
+  };
 }
